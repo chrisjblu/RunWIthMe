752afcf6398b2308b6eb425452ab37cb45eff3d4
JeganFM <44295337+JeganFM@users.noreply.github.com>
2019-04-11 21:19:07 +0100

    Adding a Splash to the application

---
+++ app/src/main/java/com/example/runwithme/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.runwithme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(3000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(SplashActivity.this,
+                            MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
